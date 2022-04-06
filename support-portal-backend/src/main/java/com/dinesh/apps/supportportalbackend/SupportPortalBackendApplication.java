057127607649bda5390166651bc424e185aeef35
dineshb1710 <dinesh.bhardwaj1710@gmail.com>
2022-04-06 11:49:02 +0530

    Step 1 : Initial Setup -> Added Backend Project.

---
+++ support-portal-backend/src/main/java/com/dinesh/apps/supportportalbackend/SupportPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.apps.supportportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportPortalBackendApplication.class, args);
+	}
+
+}
---
+++ support-portal-backend/src/test/java/com/dinesh/apps/supportportalbackend/SupportPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.apps.supportportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
